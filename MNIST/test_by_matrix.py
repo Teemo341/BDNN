5a77e573c436d92edaf2de7fac376c5fcde4112b
341 <838839085@qq.com>
2021-08-20 12:02:22 +0800

    prepare for condusion, confusion matrix remained

--- MNIST/test_by_matrix.py
+++ MNIST/test_by_matrix.py
@@ -24,7 +24,7 @@ import numpy as np
 
 # Training settings
 parser = argparse.ArgumentParser(
-    description='Test code - measure the detection peformance by variance')
+    description='Test code - measure the detection peformance by confusion matrix')
 parser.add_argument('--eva_iter', default=10, type=int,
                     help='number of passes when evaluation')
 parser.add_argument('--network', type=str, choices=['resnet', 'resnet_bayesian',
@@ -34,8 +34,10 @@ parser.add_argument('--seed', type=int, default=0, help='random seed')
 parser.add_argument('--dataset', required=True, help='in domain dataset')
 parser.add_argument('--imageSize', type=int, default=28,
                     help='the height / width of the input image to network')
+parser.add_argument('--semi_out_dataset', required=True,
+                    help='semi-out-of-dist dataset: cifar10 | svhn | imagenet | lsun')
 parser.add_argument('--out_dataset', required=True,
-                    help='out-of-dist dataset: cifar10 | svhn | imagenet | lsun')
+                    help='full-out-of-dist dataset: cifar10 | svhn | imagenet | lsun')
 parser.add_argument('--num_classes', type=int, default=10,
                     help='number of classes (default: 10)')
 parser.add_argument('--pre_trained_net', default='',
@@ -47,7 +49,7 @@ parser.add_argument('--test_batch_size', type=int, default=1000)
 args = parser.parse_args()
 print(args)
 
-outf = 'test/test_by_var/'+args.network
+outf = 'test/test_by_matrix/'+args.network
 
 if not os.path.isdir(outf):
     os.makedirs(outf)
@@ -85,6 +87,10 @@ print('load target data: ', args.dataset)
 _, test_loader = data_loader.getDataSet(
     args.dataset, args.batch_size, args.test_batch_size, args.imageSize)
 
+print('load semi data: ', args.dataset)
+semi_train_loader, semi_test_loader = data_loader.getDataSet(
+    args.semi_out_dataset, args.batch_size, args.test_batch_size, args.imageSize)
+
 print('load non target data: ', args.out_dataset)
 nt_train_loader, nt_test_loader = data_loader.getDataSet(
     args.out_dataset, args.batch_size, args.test_batch_size, args.imageSize)
@@ -111,11 +117,36 @@ def generate_target():
     correct = 0
     total = 0
     f1 = open('%s/confidence_Base_In.txt' % outf, 'w')
-    f3 = open('%s/confidence_Base_Succ.txt' % outf, 'w')
-    f4 = open('%s/confidence_Base_Err.txt' % outf, 'w')
-
     with torch.no_grad():
         for data, targets in test_loader:
+            total += data.size(0)
+            data, targets = data.to(device), targets.to(device)
+            batch_output = 0
+            batch_std = 0
+            batch_output,batch_std = evaluate(model,data)
+            # compute the accuracy
+            _, predicted = batch_output.max(1)
+            for i in range(data.size(0)):
+                # confidence score: var_y
+                # large var mean low confidence, invert var to keep pace with p(y|x)
+                std = batch_std[i,predicted[i]].item()
+                std = -std
+                f1.write("{}\n".format(std))
+    f1.close()
+
+
+def generate_semi_target():
+    model.eval()
+    if args.network == 'mc-dropout':
+        model.apply(apply_dropout)
+    correct = 0
+    total = 0
+    f2 = open('%s/confidence_Base_semi.txt' % outf, 'w')
+    f2_0 = open('%s/confidence_Base_semi_Succ.txt' % outf, 'w')
+    f2_1 = open('%s/confidence_Base_semi_Err.txt' % outf, 'w')
+
+    with torch.no_grad():
+        for data, targets in semi_test_loader:
             total += data.size(0)
             data, targets = data.to(device), targets.to(device)
             batch_output = 0
@@ -130,16 +161,17 @@ def generate_target():
                 # large var mean low confidence, invert var to keep pace with p(y|x)
                 std = batch_std[i,predicted[i]].item()
                 std = -std
-                f1.write("{}\n".format(std))
+                f2.write("{}\n".format(std))
                 if correct_index[i] == 1:
-                    f3.write("{}\n".format(std))
+                    f2_0.write("{}\n".format(std))
                 elif correct_index[i] == 0:
-                    f4.write("{}\n".format(std))
-    f1.close()
-    f3.close()
-    f4.close()
+                    f2_1.write("{}\n".format(std))
+    f2.close()
+    f2_0.close()
+    f2_1.close()
     print('\n Final Accuracy: {}/{} ({:.2f}%)\n '.format(correct,
                                                          total, 100. * correct / total))
+
 
 
 def generate_non_target():
@@ -147,7 +179,7 @@ def generate_non_target():
     if args.network == 'mc-dropout':
         model.apply(apply_dropout)
     total = 0
-    f2 = open('%s/confidence_Base_Out.txt' % outf, 'w')
+    f3 = open('%s/confidence_Base_Out.txt' % outf, 'w')
     with torch.no_grad():
         for data, targets in nt_test_loader:
             total += data.size(0)
@@ -161,8 +193,8 @@ def generate_non_target():
                 # large var mean low confidence, invert var to keep pace with p(y|x)
                 std = batch_std[i,predicted[i]].item()
                 std = -std
-                f2.write("{}\n".format(std))
-    f2.close()
+                f3.write("{}\n".format(std))
+    f3.close()
 
 
 print('generate log from in-distribution data')
