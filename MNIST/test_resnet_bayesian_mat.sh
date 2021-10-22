1478d7e41583c33d27f386189f44e33dd7aa6a87
341_lico <838839085@qq.com>
2021-10-22 08:22:25 +0800

    no

--- MNIST/test_resnet_bayesian_mat.sh
+++ MNIST/test_resnet_bayesian_mat.sh
@@ -5,7 +5,7 @@
 #SBATCH --qos=debug               
 #SBATCH -N 1                
 #SBATCH --ntasks-per-node=1                    
-#SBATCH --cpus-per-task=5
+#SBATCH --cpus-per-task=10
 #SBATCH --gres=gpu:1   
 #SBATCH -t 24:00:00 
 #SBATCH -w node4
