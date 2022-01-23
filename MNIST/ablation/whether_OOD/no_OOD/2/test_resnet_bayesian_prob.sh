7ee7fb7aeddb6ff9215eaa22e414da923c97b867
341_from_pb <838839085@qq.com>
2022-01-23 19:28:36 +0800

    ablation_OOD

---
+++ MNIST/ablation/whether_OOD/no_OOD/2/test_resnet_bayesian_prob.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+#SBATCH -J no_ood2
+#SBATCH -o result/BDNN_simple_test_prob.out               
+#SBATCH -p compute                  
+#SBATCH --qos=debug               
+#SBATCH -N 1                
+#SBATCH --ntasks-per-node=1                    
+#SBATCH --cpus-per-task=12      
+#SBATCH -t 24:00:00 
+
+
+python -u test_by_probability.py --pre_trained_net save_resnet_bayesian_mnist/final_model --network resnet_bayesian --dataset mnist --out_dataset svhn 
---
+++ MNIST/ablation/whether_OOD/no_OOD/3/train_resnet_bayesian.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+#SBATCH -J no_ood3
+#SBATCH -o result/BDNN_simple_train.out               
+#SBATCH -p compute                  
+#SBATCH --qos=debug               
+#SBATCH -N 1                
+#SBATCH --ntasks-per-node=1                    
+#SBATCH --cpus-per-task=12       
+#SBATCH -t 24:00:00 
+
+python -u resnet_bayesian_mnist.py
