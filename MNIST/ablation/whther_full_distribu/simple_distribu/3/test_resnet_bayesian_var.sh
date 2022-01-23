ffcc09459b08f657afdd530b0a1d62ca87a88ede
341_from_pb <838839085@qq.com>
2022-01-23 17:30:08 +0800

    ablation_simple

---
+++ MNIST/ablation/whther_full_distribu/simple_distribu/3/test_resnet_bayesian_var.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+#SBATCH -J simple3
+#SBATCH -o result/BDNN_simple_test_var.out               
+#SBATCH -p compute                  
+#SBATCH --qos=debug               
+#SBATCH -N 1                
+#SBATCH --ntasks-per-node=1                    
+#SBATCH --cpus-per-task=12       
+#SBATCH -t 24:00:00 
+
+
+python -u test_by_var.py --pre_trained_net save_resnet_bayesian_mnist/final_model --network resnet_bayesian --dataset mnist --out_dataset svhn 
