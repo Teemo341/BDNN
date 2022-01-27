42d527a4430cc2216e0c09e707167e2fafdd0342
341_from_pb <838839085@qq.com>
2022-01-27 14:21:59 +0800

    no

--- MNIST/test_resnet.sh
+++ MNIST/test_resnet.sh
@@ -3,9 +3,11 @@
 #SBATCH -o result/resnet_test.out                
 #SBATCH -p compute                  
 #SBATCH --qos=debug               
-#SBATCH -N 1                     
+#SBATCH -N 1                
 #SBATCH --ntasks-per-node=1                    
-#SBATCH --cpus-per-task=20  
+#SBATCH --cpus-per-task=12      
+#SBATCH -t 24:00:00 
+ 
      
 #SBATCH -t 24:00:00 
-python -u /home/siu170066/SDE-Net-master/MNIST/test_detection.py --pre_trained_net save_resnet_mnist/final_model --network resnet --dataset mnist --out_dataset svhn 
+python -u test_by_probability.py --pre_trained_net save_resnet_mnist/final_model --network resnet --dataset mnist --out_dataset svhn 
