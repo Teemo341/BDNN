105248e46c502c8b107aa49554000400437ea719
341_from_pb <838839085@qq.com>
2023-01-08 22:47:39 +0800

    start cat-dog

---
+++ additional_test/OE/test_resnet_bayesian_prob.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+#SBATCH -J OE
+#SBATCH -o result/OE_test_prob.out               
+#SBATCH -p test                  
+#SBATCH --qos=normal               
+#SBATCH -N 1                
+#SBATCH --ntasks-per-node=1                    
+#SBATCH --cpus-per-task=12      
+
+
+python -u test_by_probability.py --pre_trained_net save_resnet_OE_mnist/final_model --network resnet --dataset svhn --out_dataset cifar10
---
+++ additional_test/WOODS/test_resnet_bayesian_prob.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+#SBATCH -J WOODS
+#SBATCH -o result/WOODS_test_prob.out               
+#SBATCH -p test                  
+#SBATCH --qos=normal               
+#SBATCH -N 1                
+#SBATCH --ntasks-per-node=1                    
+#SBATCH --cpus-per-task=12      
+
+
+python -u test_by_probability.py --pre_trained_net save_resnet_WOODS_mnist/final_model --network resnet --dataset svhn --out_dataset cifar10
